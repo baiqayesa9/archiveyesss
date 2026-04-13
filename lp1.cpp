32fbd9ef41cbf1daae899b71af1575eb311ece9c
baiqayesa9 <2500018232@webmail.uad.ac.id>
2026-04-13 16:16:34 +0700

    Add files via upload

---
+++ lp1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+	
+	int panjang;
+	int lebar;
+	int luas;
+	
+	cout << "masukkan panjang : " ;
+	cin >> panjang;
+	cout << "masukkan lebar : " ;
+	cin >> lebar;
+	
+	luas = panjang * lebar;
+	
+	cout << "luas : " << luas << endl;
+	
+	return 0;
+	
+}
+
---
+++ lp2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+	
+	int a, b, c;
+	float rata;
+	
+	cout << "masukkan nilai a: ";
+	cin >> a;
+	cout << "masukkan nilai b: ";
+	cin >> b;
+	cout << "masukkan nilai c: ";
+	cin >> c;
+	
+	rata = (a+b+c) / 3.0;
+	
+	cout << "rata rata : " <<rata<<endl;
+	
+	return 0;
+	
+	
+}
